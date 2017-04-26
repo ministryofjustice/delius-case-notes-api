9d05a5023e3dacac5b1f4b750a154d786daebb56
mikejackson <michael.jackson@digital.justice.gov.uk>
2017-04-26 14:48:49 +0100

    [MJ] Set content type idiomatically, tidy up

--- src/main/java/uk/gov/jusice/digital/noms/delius/DeliusCaseNotesAPI.java
+++ src/main/java/uk/gov/jusice/digital/noms/delius/DeliusCaseNotesAPI.java
@@ -7,6 +7,8 @@ import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.name.Names;
 import org.eclipse.jetty.http.HttpStatus;
+import spark.Request;
+import spark.Response;
 import spark.Spark;
 import uk.gov.jusice.digital.noms.delius.config.Configuration;
 import uk.gov.jusice.digital.noms.delius.config.ObjectMapperFactory;
@@ -17,6 +19,7 @@ import uk.gov.jusice.digital.noms.delius.service.Service;
 
 import java.util.Optional;
 
+import static spark.Spark.before;
 import static spark.Spark.exception;
 import static spark.Spark.port;
 import static spark.Spark.post;
@@ -37,7 +40,9 @@ public class DeliusCaseNotesAPI {
 
         port(injector.getInstance(Key.get(Integer.class, Names.named("port"))));
 
-        post("/casenote/:nomisId/:noteId", (req, res) ->
+        before((request, response) -> response.type("application/json"));
+
+        post("/casenote/:nomisId/:noteId", (Request req, Response res) ->
                 {
                     CaseNoteBody caseNoteBody = objectMapper.readValue(req.body(), CaseNoteBody.class);
                     CaseNote caseNote = CaseNote.builder()
@@ -52,9 +57,9 @@ public class DeliusCaseNotesAPI {
                     int status = created.isPresent() ? HttpStatus.CREATED_201 : HttpStatus.INTERNAL_SERVER_ERROR_500;
                     res.status(status);
 
-                    return created.isPresent() ? created.get() : null;
+                    return created.orElse(null);
                 }
-        );
+        , objectMapper::writeValueAsString);
 
         exception(JsonMappingException.class, (e, req,res) -> {
             res.status(HttpStatus.BAD_REQUEST_400);
